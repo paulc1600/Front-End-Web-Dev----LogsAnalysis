01e4277e200de0f4e03f2c49d676368081fdf52f
Paul <35120590+paulc1600@users.noreply.github.com>
2018-04-30 11:05:31 -0700

    Cleaned up PEP8 issues

--- LogsReportTool.py
+++ LogsReportTool.py
@@ -16,9 +16,10 @@
 # -----------------------------------------------------------------
 #  PPC | 04/20/2018 | Dummy database mockup code
 #  PPC | 04/21/2018 | Dump completed report directly to console
+#  PPC | 04/30/2018 | Cleaned up PEP8 issues
 # -----------------------------------------------------------------
 #
-import psycopg2   
+import psycopg2
 import datetime
 import calendar
 import re
@@ -30,7 +31,7 @@ report_page = '''
   Purpose:   A reporting tool that prints out database reports (in plain text)
   Database:  newsdata.sql
   Date:      {report_date}
-  
+
 ===============================================================================
 1. What are the most popular three articles of all time? Which articles have
    been accessed the most?
@@ -49,15 +50,18 @@ report_page = '''
 ===============================================================================
 '''
 
+
 def open_report_page():
     # The text content for the report page
     content = ''
-    
+
     # Build Date String for Report
     now = datetime.datetime.now()
-    my_date = calendar.month_abbr[now.month]+' '+str(now.day)+', '+str(now.year)
-	
-    # Access database, answer 1st question: What are the most popular three articles?
+    my_date = (calendar.month_abbr[now.month] +
+               ' ' + str(now.day) +
+               ', ' + str(now.year))
+
+    # Access database, answer 1st question: Show the most popular 3 articles?
     conn = psycopg2.connect("dbname=news")
     cursor = conn.cursor()
     cursor.execute("SELECT articles.title, viewstable.views FROM articles JOIN viewstable on articles.slug = viewstable.slugpath ORDER BY viewstable.views DESC limit 3")
@@ -66,54 +70,65 @@ def open_report_page():
     # Answer 2nd question: Who are the most popular article authors?
     cursor.execute("SELECT authors.name, SUM(authorstable.views) as TotalViews FROM authors JOIN authorstable ON authorstable.author = authors.id GROUP BY authors.name ORDER BY TotalViews DESC")
     q2_rows_list = cursor.fetchall()
-	
-    # Answer 3rd question: On which days did more than 1% of requests lead to errors?
+
+    # Answer 3rd question: On which days more than 1% of req lead to errors?
     very_big_SQL = '''
-    Select mydate, pcterrors FROM ( 
+    Select mydate, pcterrors FROM (
         SELECT
-	       MyDate,  
-	       Count(Mybad) as ReqErrors, 
-	       Count(Mytotal) as ReqTotals,
-	       round(CAST(float8 (Count(Mybad) / Count(Mytotal)::float) * 100.0 as numeric), 2) as PctErrors
+           MyDate,
+           Count(Mybad) as ReqErrors,
+           Count(Mytotal) as ReqTotals,
+           round(CAST(float8 (Count(Mybad) / Count(Mytotal)::float) * 100.0 as numeric), 2) as PctErrors
 	FROM (
-	      SELECT
-		   myDate, 
-		   CASE WHEN Mybad = 1 and Mytotal = 1 THEN 1 END Mybad,
-		   CASE WHEN Mytotal = 1 THEN 1 END Mytotal
-	      FROM StatusCounts
-	) StatusCounts Group By mydate Order By PctErrors DESC) AS totalerrors 
+          SELECT
+           myDate,
+           CASE WHEN Mybad = 1 and Mytotal = 1 THEN 1 END Mybad,
+           CASE WHEN Mytotal = 1 THEN 1 END Mytotal
+          FROM StatusCounts
+    ) StatusCounts Group By mydate Order By PctErrors DESC) AS totalerrors
     where pcterrors > 1
     '''
     cursor.execute(very_big_SQL)
-    q3_rows_list = cursor.fetchall()	
-	
+    q3_rows_list = cursor.fetchall()
+
     # Build Question Answer 1 String for Report
-    q1_results_str = ""    
+    q1_results_str = ""
     for q1_results_row in q1_rows_list:
-        q1_results_str = q1_results_str + '\t' + str(q1_results_row[0]) + ' -- ' + str(q1_results_row[1]) + ' views ' + '\n'
+        q1_results_str = (q1_results_str +
+                          '\t' + str(q1_results_row[0]) +
+                          ' -- ' + str(q1_results_row[1]) +
+                          ' views ' + '\n')
 
     # Build Question Answer 2 String for Report
-    q2_results_str = ""    
+    q2_results_str = ""
+
     for q2_results_row in q2_rows_list:
-	q2e0_results_padded = str(q2_results_row[0]).ljust(32)
-        q2_results_str = q2_results_str + '\t' + q2e0_results_padded + ' -- ' + str(q2_results_row[1]) + ' views ' + '\n'	
+        q2e0_results_padded = str(q2_results_row[0]).ljust(32)
+        q2_results_str = (q2_results_str +
+                          '\t' + q2e0_results_padded +
+                          ' -- ' + str(q2_results_row[1]) +
+                          ' views ' + '\n')
 
     # Build Question Answer 3 String for Report
-    q3_results_str = ""    
+    q3_results_str = ""
+
     for q3_results_row in q3_rows_list:
-	q3_results_str = q3_results_str + '\t' + str(q3_results_row[0]) + '\t -- ' + str(q3_results_row[1]) + '% errors ' + '\n'
-	
+        q3_results_str = (q3_results_str +
+                          '\t' + str(q3_results_row[0]) +
+                          '\t -- ' + str(q3_results_row[1]) +
+                          '% errors ' + '\n')
+
     # Fill in text report template with all built strings and database answers
     content += report_page.format(
-        report_date = my_date,
-	q1_results = q1_results_str,
-        q2_results = q2_results_str,
-        q3_results = q3_results_str
+        report_date=my_date,
+        q1_results=q1_results_str,
+        q2_results=q2_results_str,
+        q3_results=q3_results_str
     )
 
     # No longer need database
     conn.close()
-	
+
     # Create or overwrite the output file
     output_file = open('newsdata_report.txt', 'w')
 
@@ -121,11 +136,12 @@ def open_report_page():
     output_file.write(content)
     output_file.close()
 
-    # read the output file to the console 
+    # read the output file to the console
     with open('newsdata_report.txt', 'r') as fin:
-    	print fin.read()
+        print fin.read()
+
     fin.close()
-	
+
 # Main Path Code Here
 open_report_page()                   # In this file
 
